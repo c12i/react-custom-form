b7712b71f4c0a665706574984631144fc587a29f
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-06-09 11:23:53 +0300

    chore (mod) - move GlobalStyles from components dir

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import styled from 'styled-components'
 
 import { Form, Input } from './components'
-import GlobalStyles from './components/GlobalStyles'
+import GlobalStyles from './GlobalStyles'
 
 const Layout = styled.div`
     h2 {
